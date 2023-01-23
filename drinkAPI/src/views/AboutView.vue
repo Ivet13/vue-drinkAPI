704bdeca206e841358d4327f719d427439b88501
Ivet13 <jazzjackrabbit13@gmail.com>
2023-01-23 20:37:53 +0100

    test fetch drink api

--- drinkAPI/src/views/AboutView.vue
+++ drinkAPI/src/views/AboutView.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <div v-if="items != null" class="">
+
+      <div v-for="item in items" :key="item" class="">
+        <div class="">
+          {{ item.idDrink }}        
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,3 +22,50 @@
   }
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [],
+    }
+  },
+  methods: {
+    async initData() {
+      //const response = await fetch(`https://travel-dummy-api.netlify.app/${this.$route.params.slug}`)
+
+      const requestOptions = {
+        method: 'GET',
+        //headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') },
+
+      };
+
+      fetch(`www.thecocktaildb.com/api/json/v1/1/random.php`, requestOptions)
+        .then(async response => {
+          const data = await response.json();
+
+          // check for error response
+          if (!response.ok) {
+            console.log('items response error')
+            // get error message from body or default to response status
+            const error = (data && data.message) || response.status;
+            return Promise.reject(error);
+          }
+          console.log('item found')
+          console.log(data)
+          this.items = data;
+
+        })
+        .catch(error => {
+          this.errorMessage = error;
+          console.error('There was an error!', error);
+        });
+
+    }
+
+  },
+  async created() {
+    this.initData()
+  }
+}
+</script>
